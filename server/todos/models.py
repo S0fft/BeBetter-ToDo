efc58f03674b26a1a5f39c7484d7796a0fccab1a
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-03-21 15:53:33 +0100

    feat: create models for Todo and TrashBin

--- server/todos/models.py
+++ server/todos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+    description = models.TextField()
+    is_done = models.BooleanField()
+    is_trashed = models.BooleanField()
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class TrashBin(models.Model):
+    title = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    category = models.CharField(max_length=128)
+    time_trashed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Removed: {self.title} at {self.time_trashed}'
