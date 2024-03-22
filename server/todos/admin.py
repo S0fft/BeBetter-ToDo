e2537f4911af5fddb3ca6f91f74550304d9f6de4
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-03-22 17:39:14 +0100

    feat: add admin settings

--- server/todos/admin.py
+++ server/todos/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from todos.models import Todo, TrashBin
 
-admin.site.register(Todo)
+# admin.site.register(Todo)
 admin.site.register(TrashBin)
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'time_created', 'time_updated']
+
+    # fields = ['image', 'name', 'description', ('price', 'quantity'), 'stripe_product_price_id', 'category']
+    # search_field = ['name', 'category__name']
+    # ordering = ['name']
