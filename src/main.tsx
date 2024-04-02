29246637b389748f3cc127149c9f3eca1fe7c47b
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-02 15:44:15 +0300

    refactor: remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 import './index.css';
-import { DarkModeProvider } from '@features/AppTheme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <App />
   </React.StrictMode>,
 );
