ac6191215b8e506153cdec0d48bfa6fd51bd4b58
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-03 22:07:03 +0300

    refactor: change to use semantic html

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 const AppLayout = () => {
   return (
     <main className="grid h-dvh grid-cols-[300px_1fr_1fr] grid-rows-1 gap-6 p-4 text-4xl text-on-surface">
-      <section className="w-[300px]">sidebar</section>
+      <aside className="w-[300px]">sidebar</aside>
       <div className="grid grid-rows-[64px_1fr] gap-3">
-        <section>header</section>
+        <header>header</header>
         <section>
           <Outlet />
         </section>
