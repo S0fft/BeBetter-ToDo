6791b13fa780832345de048be2e7ba4db2d67843
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-08 14:54:23 +0300

    fix: animation

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -32,7 +32,7 @@ const AppLayout = () => {
       </div>
       <section
         className={cn(
-          'grid scale-95 grid-rows-[max-content_1fr] overflow-hidden rounded-xl opacity-0 transition-all delay-[250ms]',
+          'grid scale-95 grid-rows-[max-content_1fr] overflow-hidden rounded-xl opacity-0 transition-all delay-[50ms] duration-200',
           {
             'scale-100 opacity-100 delay-0': isNoteExpanded,
           },
