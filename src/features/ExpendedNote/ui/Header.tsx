32c3726f57f8349884971e16e14fb76cffbb8c52
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-09 09:25:26 +0300

    fix: border radius issue

--- src/features/ExpendedNote/ui/Header.tsx
+++ src/features/ExpendedNote/ui/Header.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 const Header: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <header className="flex items-center gap-4 bg-surface-container pb-5 pl-7 pr-1 pt-6">
+    <header className="flex items-center gap-4 rounded-t-xl bg-surface-container pb-5 pl-7 pr-1 pt-6">
       {children}
     </header>
   );
