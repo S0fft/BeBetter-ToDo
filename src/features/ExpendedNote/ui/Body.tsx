a20fb07729133c8dc10db8659bd2c1e295d9907b
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-10 14:58:54 +0300

    fix: note collapse padding

--- src/features/ExpendedNote/ui/Body.tsx
+++ src/features/ExpendedNote/ui/Body.tsx
@@ -10,7 +10,7 @@ const Body: FC<PropsWithChildren> = ({ children }) => {
   const activeNote = Number(readUrl(urlParams.NOTE_ID));
 
   return (
-    <article className="relative grid h-full w-full grid-rows-[max-content_1fr] gap-5 bg-surface p-6 transition-all before:absolute before:left-8 before:top-1 before:flex before:size-fit before:items-center before:justify-center before:bg-surface before:px-2 before:text-sm before:text-on-surface-variant before:opacity-0 before:transition-all before:content-['Title'] has-[input:focus]:before:opacity-100">
+    <article className="relative grid h-full w-full grid-rows-[max-content_1fr] gap-5 rounded-b-xl bg-surface p-6 transition-all before:absolute before:left-8 before:top-1 before:flex before:size-fit before:items-center before:justify-center before:bg-surface before:px-2 before:text-sm before:text-on-surface-variant before:opacity-0 before:transition-all before:content-['Title'] has-[input:focus]:before:opacity-100">
       <Title key={`title-${activeNote}`} />
       <Content key={`content-${activeNote}`} />
       {children}
--- src/layout/AppLayout/AppLayout.tsx
+++ src/layout/AppLayout/AppLayout.tsx
@@ -18,7 +18,7 @@ const AppLayout = () => {
   return (
     <main
       className={cn(
-        'grid h-dvh grid-cols-[300px_1fr_1fr] grid-rows-1 gap-6 px-4 pb-4 text-on-surface transition-all duration-400 ease-emphasized-decelerate',
+        'grid h-dvh grid-cols-[300px_1fr_1fr] grid-rows-1 gap-6 pb-4 pl-4 text-on-surface transition-all duration-400 ease-emphasized-decelerate',
         {
           'grid-cols-[300px_1fr_0fr] ease-emphasized-accelerate':
             !isNoteExpanded,
@@ -44,7 +44,7 @@ const AppLayout = () => {
       </section>
       <section
         className={cn(
-          'grid scale-95 grid-rows-[max-content_1fr] overflow-hidden rounded-xl pt-4 opacity-0 transition-all delay-[50ms] duration-200',
+          'grid scale-95 grid-rows-[max-content_1fr] overflow-hidden pr-4 pt-4 opacity-0 transition-all delay-[50ms] duration-200',
           {
             'scale-100 opacity-100 delay-0': isNoteExpanded,
           },
