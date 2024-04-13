927fe3b33e97ab64bfc65fd9b479109cae2874e8
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-13 16:23:02 +0300

    feat: implement to hide empty trash button if list is empty

--- src/pages/Trash/Trash.tsx
+++ src/pages/Trash/Trash.tsx
@@ -5,13 +5,16 @@ import TextButton from '@shared/ui/TextButton';
 
 const Trash = () => {
   const notes = useAppSelector(selectNotes);
+
   const trashedNotes = notes.filter(({ isTrashed }) => isTrashed);
+  const isTrashNotEmpty = trashedNotes.length !== 0;
+
   return (
     <NotesList
       preList={
         <article className="flex items-center justify-center gap-3 py-6 font-medium">
-          <h4>Notes in Trash are deleted after 7 days.</h4>{' '}
-          <TextButton>Empty Trash</TextButton>
+          <h4>Notes in Trash are deleted after 7 days.</h4>
+          {isTrashNotEmpty && <TextButton>Empty Trash</TextButton>}
         </article>
       }
       notes={trashedNotes}
