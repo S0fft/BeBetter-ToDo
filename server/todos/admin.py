553d0dedb1bb5c34d633551e600c4901e10c917e
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-13 12:22:58 +0200

    test changes

--- server/todos/admin.py
+++ server/todos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from todos.models import Todo
+from todos.models import Label, Todo
+
+admin.site.register(Label)
 
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ['title', 'label', 'time_created', 'time_updated']
+    list_display = ['title', 'time_created', 'time_updated']
--- server/todos/models.py
+++ server/todos/models.py
@@ -3,12 +3,22 @@ from django.utils import timezone
 from users.models import User
 
 
+class Label(models.Model):
+    title = models.CharField(max_length=128)
+    color = models.CharField(max_length=6)
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Todo(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
-    label = models.CharField(max_length=128)
+    # label = models.ForeignKey(to=Label, max_length=128, default=None, on_delete=models.PROTECT)
+    labels = models.ManyToManyField('Label', null=True, blank=True)
     content = models.TextField()
     is_done = models.BooleanField(default=False)
+    is_pined = models.BooleanField(default=False)
     is_trashed = models.BooleanField(default=False)
     is_archived = models.BooleanField(default=False)
     time_created = models.DateTimeField(auto_now_add=True)
