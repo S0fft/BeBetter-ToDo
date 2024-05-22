663f07b668b05a076d38719ecf5c117f3b9964c9
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-22 22:31:32 +0200

    add router's dynamics urls

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework import routers
 from todos.views import TodoViewSet
 
 app_name = 'todos'
 
 router = routers.DefaultRouter()
-
-router.register(r'todos', TodoViewSet, basename='todos')
+router.register(r'', TodoViewSet, basename='todos')
 
 urlpatterns = [
-    path('main/', TodoViewSet.as_view({'get': 'list'})),
-    path('main/<int:pk>/', TodoViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('', include(router.urls)),
 ]
--- server/todos/views.py
+++ server/todos/views.py
@@ -1,1 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from todos.models import Label, Todo
+from todos.serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        if not pk:
+            return Todo.objects.all()
+
+        return Todo.objects.filter(pk=pk)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    @action(methods=['get'], detail=True)
+    def label(self, request, pk=None):
+        label = Label.objects.get(pk=pk)
+
+        return Response({'Label': label.title})
