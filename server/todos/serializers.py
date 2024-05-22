89fa1c8b73927e9ea831ae8dc0b0a41b5ce3d84a
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-22 19:06:02 +0200

    create TodoSerializer, hide user field

---
+++ server/todos/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from todos.models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Todo
+        fields = "__all__"
