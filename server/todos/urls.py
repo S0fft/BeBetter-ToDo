acb3f8c8b3a3502685c5350f25f1e5049b3f2385
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-22 19:05:06 +0200

    add routers for main and main<pk>

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -1,8 +1,14 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from rest_framework import routers
+from todos.views import TodoViewSet
 
 app_name = 'todos'
 
+router = routers.DefaultRouter()
+
+router.register(r'todos', TodoViewSet, basename='todos')
+
 urlpatterns = [
-    # path('users/', include('users.urls', namespace='users')),
+    path('main/', TodoViewSet.as_view({'get': 'list'})),
+    path('main/<int:pk>/', TodoViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
 ]
