8271e0fa23253ce65e59c5614b56b56104b73bb4
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-23 20:18:34 +0200

    add filter todos by user and pk + add permission class

--- server/todos/views.py
+++ server/todos/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from todos.models import Label, Todo
 from todos.serializers import TodoSerializer
@@ -7,14 +8,16 @@ from todos.serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        user = self.request.user
         pk = self.kwargs.get('pk')
 
-        if not pk:
-            return Todo.objects.all()
+        if pk:
+            return Todo.objects.filter(pk=pk, user=user)
 
-        return Todo.objects.filter(pk=pk)
+        return Todo.objects.filter(user=user)
 
     @action(methods=['get'], detail=True)
     def label(self, request, pk=None):
