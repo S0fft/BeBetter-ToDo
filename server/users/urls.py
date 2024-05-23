2eebb7c4d6e20d1c995716a49a90766ea51bdf5c
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-23 20:11:18 +0200

    add token urls for auth/refresh/verify

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'users'
 
 urlpatterns = [
-    # path('users/', include('users.urls', namespace='users')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
