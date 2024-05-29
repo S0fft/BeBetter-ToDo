6d938e2d539d5ec87e82192542e5af363c1a24f6
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-29 11:27:34 +0200

    add auht route for djoser

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'users'
@@ -7,4 +7,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('auth/', include('djoser.urls')),
 ]
