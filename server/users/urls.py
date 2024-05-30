bd3685b47269acadbaaec6c49dc97f9846ac2b98
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-05-30 10:35:19 +0200

    create url for logout, changed routees views name

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from users import views
+
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('register/', views.RegisterView.as_view(), name='auth_register'),
-    path('profile/', views.getProfile, name='profile'),
-    path('profile/update/', views.updateProfile, name='update-profile'),
+    path('profile/', views.get_profile, name='profile'),
+    path('profile/update/', views.update_profile, name='update-profile'),
+    path('logout/', views.Logout.as_view(), name='auth_logout'),
 ]
