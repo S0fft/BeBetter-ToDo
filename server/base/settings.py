942c6264105adeafbb751f2f090c87255314e0ef
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-01 12:26:47 +0200

    add config for corsheaders

--- server/base/settings.py
+++ server/base/settings.py
@@ -19,15 +19,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
 
     'todos.apps.TodosConfig',
     'users.apps.UsersConfig',
     'rest_framework_simplejwt.token_blacklist',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
