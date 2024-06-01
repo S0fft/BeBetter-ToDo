274bd4422de11d5ce344b94beb6a6d7ca095d16d
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-01 09:14:26 +0200

    remove space

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -7,7 +7,6 @@ from .models import User
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
