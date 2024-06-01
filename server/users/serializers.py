2366aee254d407d1d8c8fb2c55e5f98fe9085be7
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-01 09:06:28 +0200

    replaced . on ! in error message

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -29,7 +29,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+            raise serializers.ValidationError({"password": "Password fields didn't match!"})
 
         return attrs
 
