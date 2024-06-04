b4353e519ea0003203e19ad6da74575e8a37edba
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-04 21:36:20 +0200

    remove some comments in files

--- server/base/settings.py
+++ server/base/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG: bool = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
