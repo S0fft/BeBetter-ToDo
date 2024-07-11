788f73eb47391612c5f058c39d6e59c90c8cc613
Bogdan <bogdanscherbinadev@gmail.com>
2024-07-11 22:38:22 +0300

    fix: redux warn

--- client/src/shared/lib/selectors/selectAccessToken.ts
+++ client/src/shared/lib/selectors/selectAccessToken.ts
@@ -3,8 +3,8 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '@/store';
 
 const selectAccessToken = createSelector(
-  (state: RootState) => state.sessionReducer.accessToken,
-  (accessToken) => accessToken,
+  (state: RootState) => state.sessionReducer,
+  (accessToken) => accessToken.accessToken,
 );
 
 export default selectAccessToken;
--- client/src/shared/lib/selectors/selectIsLogged.ts
+++ client/src/shared/lib/selectors/selectIsLogged.ts
@@ -3,8 +3,8 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '@/store';
 
 const selectIsLogged = createSelector(
-  (state: RootState) => state.sessionReducer.isLogged,
-  (isLogged) => isLogged,
+  (state: RootState) => state.sessionReducer,
+  (isLogged) => isLogged.isLogged,
 );
 
 export default selectIsLogged;
