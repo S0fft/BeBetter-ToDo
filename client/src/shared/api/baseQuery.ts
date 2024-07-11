def6a041f30cc6de5af8a13ce6a9f87755f66fa8
Bogdan <bogdanscherbinadev@gmail.com>
2024-07-11 23:38:17 +0300

    fix: Bearer typo

--- client/src/shared/api/baseQuery.ts
+++ client/src/shared/api/baseQuery.ts
@@ -6,7 +6,7 @@ const baseQuery = fetchBaseQuery({
   baseUrl: `${import.meta.env.VITE_API_URL}`,
   prepareHeaders: (headers, { getState }) => {
     const { accessToken } = (getState() as RootState).sessionReducer;
-    headers.set('Authorization', `berear ${accessToken}`);
+    headers.set('Authorization', `Bearer ${accessToken}`);
   },
 });
 
