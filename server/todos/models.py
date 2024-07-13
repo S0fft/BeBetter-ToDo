f02a00161605b30a47a2ccecb2100344a9f77178
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-07-13 12:38:41 +0200

    featur for auto-deleting (comments)

--- server/todos/models.py
+++ server/todos/models.py
@@ -1,4 +1,7 @@
+# from datetime import timedelta
+
 from django.db import models
+# from django.utils import timezone
 from users.models import User
 
 
@@ -6,7 +9,7 @@ class Label(models.Model):
     title = models.CharField(max_length=128)
     color = models.CharField(max_length=6, default=False)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
 
 
@@ -20,6 +23,28 @@ class Todo(models.Model):
     is_trashed = models.BooleanField(default=False)
     time_created = models.DateTimeField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
+    time_trashed = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
+
+    # def save(self, *args, **kwargs):
+    #     if self.is_trashed and not self.time_trashed:
+    #         self.time_trashed = timezone.now()
+    #     elif not self.is_trashed:
+    #         self.time_trashed = None
+    #     super().save(*args, **kwargs)
+
+    # @property
+    # def time_in_trash(self):
+    #     if self.is_trashed and self.time_trashed:
+    #         time_trash = timezone.now() - self.time_trashed
+    #         return time_trash
+    #     return None
+
+    # def delete_if_old_trash(self):
+    #     if self.is_trashed and self.time_in_trash and self.time_in_trash >= timedelta(seconds=7):
+    #         self.delete()
+
+    # def delete(self):
+    #     super().delete()
