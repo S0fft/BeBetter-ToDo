e95c083b21694072be28e036a5b5576f2340cacb
Bogdan <bogdanscherbinadev@gmail.com>
2024-07-13 11:42:31 +0300

    fix: mysql error

--- server/base/settings.py
+++ server/base/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
         # 'HOST': config('HOST'),
         # 'PORT': config('PORT'),
 
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-        },
+        # 'OPTIONS': {
+        #     'charset': 'utf8mb4',
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        # },
     }
 }
 
