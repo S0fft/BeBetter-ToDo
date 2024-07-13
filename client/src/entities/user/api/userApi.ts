a1c9027e027c18aa5a9d26a016b5bccf6afb1683
Bogdan <bogdanscherbinadev@gmail.com>
2024-07-13 16:14:57 +0300

    refactor: make user profile entirely cached

--- client/src/entities/user/api/userApi.ts
+++ client/src/entities/user/api/userApi.ts
@@ -4,6 +4,7 @@ import { User } from '@shared/types';
 export const userApi = rootApi.injectEndpoints({
   endpoints: (builder) => ({
     profile: builder.query<User, void>({
+      keepUnusedDataFor: Number.MAX_SAFE_INTEGER,
       query: () => ({
         url: '/users/profile/',
       }),
