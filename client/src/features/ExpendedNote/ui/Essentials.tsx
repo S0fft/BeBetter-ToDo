e37590be5838a70de147afac97d781814bb4d50e
Bogdan <bogdanscherbinadev@gmail.com>
2024-07-13 15:37:44 +0300

    feat: implement to show actual note date

--- client/src/features/ExpendedNote/ui/Essentials.tsx
+++ client/src/features/ExpendedNote/ui/Essentials.tsx
@@ -1,3 +1,4 @@
+import CreatedAt from '@features/ExpendedNote/ui/CreatedAt';
 import { urlParams } from '@shared/lib/const';
 import useUrl from '@shared/lib/hooks/useUrl';
 import Labels from '@shared/ui/Labels';
@@ -8,12 +9,13 @@ const Essentials = () => {
   const { readUrl } = useUrl();
 
   const activeNote = Number.parseInt(readUrl(urlParams.NOTE_ID), 10);
+  // TODO: make real labels
   const labels = mockedNotes?.[activeNote]?.labels;
 
   return (
     <article className="grid gap-1">
       <h2 className="text-xl font-medium">Caroline</h2>
-      <h3>Created at 20.03.24</h3>
+      <CreatedAt />
       <Labels className="mt-1 w-full self-start" labels={labels} />
     </article>
   );
