f1e62b9fc08d7e910419a5cdfa3ae59168b56d60
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-07-20 12:36:04 +0200

    add default param to image field

--- server/users/models.py
+++ server/users/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    image = models.ImageField(upload_to='users_images', null=True, blank=True, default='default_avatar.png')
     is_verified_email = models.BooleanField(default=False)
     registration_date = models.DateTimeField(auto_now_add=True)
