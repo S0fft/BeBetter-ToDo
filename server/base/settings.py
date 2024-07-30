d99563ce12ab6252daf7ee281fddb7821d47cd94
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-07-30 10:34:46 +0200

    remove unnecsecery code

--- server/base/settings.py
+++ server/base/settings.py
@@ -161,10 +161,3 @@ SIMPLE_JWT = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Адрес брокера сообщений
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # Адрес результата
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
