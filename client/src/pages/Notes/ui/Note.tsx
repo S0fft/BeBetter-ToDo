e8d2b723fe6394a3f13cae8401f58d1b652641e3
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-08-20 16:19:40 +0300

    refactor: change note pad color

--- client/src/pages/Notes/ui/Note.tsx
+++ client/src/pages/Notes/ui/Note.tsx
@@ -92,7 +92,7 @@ const Note: FC<NoteProps> = ({
       }}
       onClick={handleSelectNote}
       className={cn(
-        'relative flex h-[200px] w-full flex-col gap-5 rounded-xl bg-surface-container pl-6 pr-3 pt-6 text-on-surface transition-all duration-400 ease-emphasized-decelerate after:pointer-events-none after:absolute after:bottom-0 after:left-0 after:h-[90px] after:w-full after:rounded-xl after:bg-gradient-to-t after:from-surface-bright after:to-transparent after:opacity-30 hover:bg-surface-container-low',
+        'relative flex h-[200px] w-full flex-col gap-5 rounded-xl bg-surface-container pl-6 pr-3 pt-6 text-on-surface transition-all duration-400 ease-emphasized-decelerate after:pointer-events-none after:absolute after:bottom-0 after:left-0 after:h-[90px] after:w-full after:rounded-xl after:bg-gradient-to-t after:from-surface-container-lowest after:to-transparent after:opacity-30 hover:bg-surface-container-low',
         {
           '!-translate-y-0.5 bg-high-contrast-inverse-primary drop-shadow-md [box-shadow:_0px_1px_2px_0px_#0000004D] hover:bg-high-contrast-inverse-primary':
             isActiveNote,
