c06952c320ea5b645549ea7dc82026cd57dc4aa3
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-08-20 22:41:28 +0300

    fix: to dismiss old toast on new one

--- client/src/shared/lib/hooks/useSnackbar.ts
+++ client/src/shared/lib/hooks/useSnackbar.ts
@@ -1,21 +1,17 @@
-import { useRef } from 'react';
-
 import { UNKNOWN_ERROR_MESSAGE } from '@shared/lib/const';
 import isApiError from '@shared/lib/helpers/isApiError';
-import { Id, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 const useSnackbar = () => {
-  const toastIdRef = useRef<Id | null>(null);
-
   const handleUpdateMessage = (m: string) => {
-    if (toastIdRef.current) toast.dismiss(toastIdRef.current);
-    toastIdRef.current = toast(m);
+    toast.dismiss();
+    toast(m);
   };
 
   const handleErrorMessage = (e: unknown) => {
-    if (toastIdRef.current) toast.dismiss(toastIdRef.current);
+    toast.dismiss();
     const errorMessage = isApiError(e) ? e.data.detail : UNKNOWN_ERROR_MESSAGE;
-    toastIdRef.current = toast(errorMessage);
+    toast(errorMessage);
   };
 
   return {
