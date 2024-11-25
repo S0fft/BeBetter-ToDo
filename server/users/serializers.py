b62df9c11aee170d1956a83490809ad1b4a7afac
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-25 19:54:50 +0100

    delete unnecessary comment

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.serializers import ImageField
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
