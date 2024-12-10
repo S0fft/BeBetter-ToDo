254f5b0fba3a03bd03e70e28b63850b1a5236f75
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-12-10 17:38:36 +0100

    create first_name and last_name fields for user model

--- server/users/models.py
+++ server/users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     image = models.ImageField(upload_to='users_images', null=True, blank=True, default='default_avatar.png')
     is_verified_email = models.BooleanField(default=False)
     registration_date = models.DateTimeField(auto_now_add=True)
